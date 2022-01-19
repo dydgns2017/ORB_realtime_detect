8c075dc9d30280c9be68662274f9dbf7ff7001e8
Yonghoon JunG <asdf2017@naver.com>
2022-01-19 15:55:20 +0900

    update

--- lib/dataProcessing.py
+++ lib/dataProcessing.py
@@ -14,8 +14,9 @@ LD_folder = os.path.join(Root_folder, "images/LD")
 Lapla_folder = os.path.join(Root_folder, "images/Lapla")
 HSV_folder = os.path.join(Root_folder, "images/HSV")
 GRAY_folder = os.path.join(Root_folder, "images/GRAY")
-Denoise_folder = os.path.join(Root_folder, "images/Denoise")
-folders = [Canny_folder, Sobel_folder, LD_folder, Lapla_folder, RGB_folder, HSV_folder, GRAY_folder]
+Clahe_folder = os.path.join(Root_folder, "images/Clahe")
+folders = [Canny_folder, Sobel_folder, LD_folder, Lapla_folder, RGB_folder, \
+    HSV_folder, GRAY_folder, Clahe_folder]
 
 class CreateDataSet:
     def __init__(self) -> None:
@@ -26,7 +27,15 @@ class CreateDataSet:
             files = glob.glob(folder + "\\*.jpg")
             for file in files:
                 os.remove(file)
-    
+            
+    def createClahe(self):
+        global Clahe_folder
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(27,27))
+        for file in self.files:
+            temp_file = cv2.imread(file, 0)
+            cl1 = clahe.apply(temp_file)
+            cv2.imwrite(Clahe_folder + "/" + file.split("\\")[-1], cl1)
+                
     def createHSV(self):
         global HSV_folder
         for file in self.files:
@@ -39,7 +48,7 @@ class CreateDataSet:
         for file in self.files:
             temp_file = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
             # code...
-            canny = cv2.Canny(temp_file, 50, 200)
+            canny = cv2.Canny(temp_file, 80, 200)
             cv2.imwrite(Canny_folder + "/" + file.split("\\")[-1], canny)
 
     def createSobel(self): 
@@ -75,18 +84,11 @@ class CreateDataSet:
             temp_file = cv2.cvtColor(temp_file, cv2.COLOR_BGR2GRAY)
             cv2.imwrite(GRAY_folder + "/" + file.split("\\")[-1], temp_file)
     
-    def createDenoise(self): # with histogram
-        global Denoise_folder
-        for file in self.files:
-            temp_file = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-            image = cv2.fastNlMeansDenoising(temp_file, None)
-            cv2.imwrite(Denoise_folder + "/" + file.split("\\")[-1], image)
-    
     def createStart(self):
         self.files = glob.glob(".\\images\\origin\\*.jpg")
         for file in self.files:
             f = cv2.imread(file)
-            f = cv2.resize(f, (680, 540))
+            f = cv2.resize(f, (540, 540))
             cv2.imwrite(RGB_folder + "/" + file.split("\\")[-1], f)
         self.files = glob.glob(".\\images\\RGB\\*.jpg")
         self.createLapla()
@@ -95,14 +97,14 @@ class CreateDataSet:
         self.createCanny()
         self.createHSV()
         self.createGray()
-        self.createDenoise()
-
+        self.createClahe()
+        
 class ORBDetection:
     def __init__(self, select_folder) -> None:
-        self.orb_detector = cv2.ORB_create(30000)
+        self.orb_detector = cv2.ORB_create(5000)
         self.cap = cv2.VideoCapture(0)
         # 클래스 변경시 변경.
-        self.match_files = [[cv2.imread(file), file] for file in glob.glob(select_folder + "/*.jpg")]
+        self.match_files = [[cv2.imread(file), file] for file in glob.glob(select_folder + "/class_B*.jpg")]          
         self.bf = cv2.BFMatcher_create(cv2.NORM_HAMMING, crossCheck=True)
         # target image features extract
         self.features = []
@@ -111,10 +113,12 @@ class ORBDetection:
             kp2, desc2 = self.orb_detector.detectAndCompute(file, None)
             self.features.append([kp2, desc2, file, filename])
 
-        self.orb_detector = cv2.ORB_create(200)
-        self.target_fps = 60
+        self.orb_detector = cv2.ORB_create(500)
+        self.target_fps = 20
         self.stable_stack = []
         self.target_feature = None
+   
+
 
     def mostFrequent(self):
         most = max(set(self.stable_stack), key=self.stable_stack.count)
@@ -131,7 +135,7 @@ class ORBDetection:
         for feature in features:
             kp2, desc2, file, filename = feature # unpacking
             matches = self.bf.match(self.desc1, desc2)
-            matches = sorted(matches[:30], key = lambda x:x.distance)
+            matches = sorted(matches[:50], key = lambda x:x.distance)
             L = [sum([x.distance for x in matches]), kp2, file, matches, filename]
             dst.append(L)
         #dst = sorted(dst, key=lambda x:x[0], reverse=True)
@@ -160,7 +164,7 @@ class ORBDetection:
             matches = self.bf.match(self.desc1, desc2)
             matches = sorted(matches, key = lambda x:x.distance)
             res = cv2.drawMatches(frame, self.kp1, file, kp2, matches, None, \
-                flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
             cv2.namedWindow("res", cv2.WINDOW_NORMAL)
             cv2.imshow("res", res)
             key = cv2.waitKey(1)
@@ -183,12 +187,18 @@ def getSobel(frame):
 
 def getCanny(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return cv2.Canny(frame, 50, 200)
+    return cv2.Canny(frame, 80, 200)
 
 def getHSV(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     return frame
 
+def getClahe(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(27,27))
+    cl1 = clahe.apply(frame)
+    return cl1
+
 def getGRAY(frame):
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.uint8)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return frame
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from lib.dataProcessing import *
 if __name__ == "__main__":
     data = CreateDataSet()
     data.createStart() ## 데이터 생성 진행
-    ## RGB_folder Canny_folder Sobel_folder LD_folder Lapla_folder HSV_folder GRAY_folder
-    detect = ORBDetection(GRAY_folder)
-    ## getCanny getLapla getSobel getLD getHSV getGRAY
-    # detect.startDetect(getGRAY)
+    ## RGB_folder Canny_folder Sobel_folder LD_folder Lapla_folder HSV_folder
+    detect = ORBDetection(Clahe_folder)
+    ## getCanny getLapla getSobel getLD getHSV
+    detect.startDetect(getClahe)
